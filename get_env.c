636dae725192c846dbd8e0487841434370ddba47
Ismael Kiprop <133222922+IsmaelKiprop@users.noreply.github.com>
2023-08-24 02:57:08 -0700

    Update get_env.c

--- get_env.c
+++ get_env.c
@@ -1,89 +1,169 @@
 #include "main.h"
 /**
- * get_environ - Returns string array copy of our environ
- * @inf: Structure containing potential arguments.
- * Return: Always 0 Success
+ * input_buf - buffer chained command
+ * @info: par struct
+ * @buf:  buffer address
+ * @len:  buffer address
+ *
+ * Return: bytes read
  */
-char **get_environ(passinf *inf)
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
-	if (!inf->environ || inf->env_changed)
-	{
-		inf->environ = list_to_strings(inf->env);
-		inf->env_changed = 0;
-	}
+	ssize_t r = 0;
+	size_t len_p = 0;
 
-	return (inf->environ);
+	if (!*len) /* if nothing left in the buffer, fill it */
+	{
+		/*bfree((void **)info->cmd_buf);*/
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			/* if (_strchr(*buf, ';')) is this a command chain? */
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
 }
 
 /**
- * unset_env - Remove environment variable
- * @inf: Structure containing potential arguments.
- *  Return: 1 when deleted, 0 otherwise
- * @var: String env var property
+ * get_input - get line minus newline
+ * @info: par struct
+ *
+ * Return: bytes read
  */
-int unset_env(passinf *inf, char *var)
+ssize_t get_input(info_t *info)
 {
-	list_str *node = inf->env;
-	size_t w = 0;
-	char *u;
+	static char *buf; /* the ';' command chain buffer */
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->arg), *p;
 
-	if (!node || !var)
-		return (0);
-
-	while (node)
+	_putchar(BUF_FLUSH);
+	r = input_buf(info, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len) /* we have commands left in the chain buffer */
 	{
-		u = starts_with(node->str, var);
-		if (u && *u == '=')
+		j = i; /* init new iterator to current buf position */
+		p = buf + i; /* get pointer for return */
+
+		check_chain(info, buf, &j, i, len);
+		while (j < len) /* iterate to semicolon or end */
 		{
-			inf->env_changed = delete_node_at_index(&(inf->env), w);
-			w = 0;
-			node = inf->env;
-			continue;
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
 		}
-		node = node->next;
-		w++;
+
+		i = j + 1; /* increment past nulled ';'' */
+		if (i >= len) /* reached end of buffer? */
+		{
+			i = len = 0; /* reset position and length */
+			info->cmd_buf_type = CMD_NORM;
+		}
+
+		*buf_p = p; /* pass back pointer to current command position */
+		return (_strlen(p)); /* return length of current command */
 	}
-	return (inf->env_changed);
+
+	*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
+	return (r); /* return length of buffer from _getline() */
 }
 
 /**
- * set_env - Initialize a new environment variable
- * @inf: Structure containing potential arguments.
- * @var: String env var property
- * @value: String env var value
- *  Return: Always 0 Success
+ * read_buf - read buffer
+ * @info: par struct
+ * @buf: buffer
+ * @i: size
+ *
+ * Return: r
  */
-int set_env(passinf *inf, char *var, char *value)
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
 {
-	char *buf = NULL;
-	list_str *node;
-	char *u;
+	ssize_t r = 0;
 
-	if (!var || !value)
+	if (*i)
 		return (0);
-
-	buf = malloc(strlen(var) + strlen(value) + 2);
-	if (!buf)
-		return (1);
-	strcpy(buf, var);
-	strcat(buf, "=");
-	strcat(buf, value);
-	node = inf->env;
-	while (node)
-	{
-		u = starts_with(node->str, var);
-		if (u && *u == '=')
-		{
-			free(node->str);
-			node->str = buf;
-			inf->env_changed = 1;
-			return (0);
-		}
-		node = node->next;
-	}
-	add_node_end(&(inf->env), buf, 0);
-	free(buf);
-	inf->env_changed = 1;
-	return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
 }
 
+/**
+ * _getline - get next line of input from STDIN
+ * @info: par struct
+ * @ptr: pointer address to buffer, preallocated or NULL
+ * @length: size of the preallocated ptr buffer if not NULL
+ *
+ * Return: s
+ */
+int _getline(info_t *info, char **ptr, size_t *length)
+{
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p) /* MALLOC FAILURE! */
+		return (p ? free(p), -1 : -1);
+
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+
+	s += k - i;
+	i = k;
+	p = new_p;
+
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
+
+/**
+ * sigintHandler - block ctrl-C
+ * @sig_num: signal number
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
