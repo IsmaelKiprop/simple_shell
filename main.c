75cc2f7b7194b9cd1fe05e753827d5bff7d29381
Ismael Kiprop <133222922+IsmaelKiprop@users.noreply.github.com>
2023-08-24 02:11:28 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,62 +1,64 @@
 #include "main.h"
 /**
- * interactivemode - If shell is in interactive mode, return true
- * @inf: Struct Address
+ * interactive - returns true if the shell is in the interactive mode
+ * @info: struct address
  *
- * Return: Always 1 if interactive mode, 0 otherwise
+ * Return: 1 if in the interactive mode, 0 otherwise
  */
-int interactivemode(passinf *inf)
+int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && inf->read_fd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delimiter - If character is a delimiter
- * @x: char check
- * @delim: delimiter string
- * Return: when true return 1, 0 if false
+ * is_delim - checks if the character is delimeter
+ * @c: char to be check
+ * @delim: the delimeter string
+ * Return: 1 if it is true, and 0 if false
  */
-int is_delimiter(char x, char *delim)
+int is_delim(char c, char *delim)
 {
 	while (*delim)
-		if (*delim++ == x)
+		if (*delim++ == c)
 			return (1);
 	return (0);
 }
 
 /**
- * is_alpha - alphabetic character to be checked
- * @x: character to check
- * Return: if x is alphabetic return 1, 0 otherwise
+ * _isalpha - checks for alphabetic characters
+ * @c: character to  be input
+ * Return: 1 if c is alphabetic, and 0 otherwise
  */
-int is_alpha(int x)
+
+int _isalpha(int c)
 {
-	if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
 
 /**
- * str_int - converts a string to an integer
- * @x: string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ * _atoi - converts the string to integer
+ * @x: string to convert
+ * Return: 0 if there is no numbers in the  string, or converted number otherwise
  */
-int str_int(char *x)
+
+int _atoi(char *x)
 {
 	int w, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (w = 0; x[w] != '\0' && flag != 2; x++)
+	for (w = 0; x[w] != '\0' && flag != 2; w++)
 	{
-		if (w[x] == '-')
+		if (x[w] == '-')
 			sign *= -1;
 
-		if (w[x] >= '0' && w[x] <= '9')
+		if (x[w] >= '0' && x[w] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (w[x] - '0');
+			result += (x[w] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
@@ -69,4 +71,5 @@ int str_int(char *x)
 
 	return (output);
 }
+
 
